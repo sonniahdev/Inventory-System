6549bd707f40d596157792502b32f490d995c34f
sonniahdev <soniahnjogu435@gmail.com>
2025-07-31 11:08:20 +0300

    Prepare for Render deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,10 @@ Rails.application.configure do
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "example.com" }
 
+
+config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+
+
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
   #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
